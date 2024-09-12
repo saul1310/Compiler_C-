ef1ef9b5eca8ed4b4b5c24ddf7d819f1d1a2a88f
Saul Ifshin <97627272+saul1310@users.noreply.github.com>
2024-09-12 10:01:56 -0700

    head pointer definitions

--- Main.cpp
+++ Main.cpp
@@ -2,4 +2,15 @@
 #include <cstring>
 #include<climits>
 
+MemoryAllocator::MemoryAllocator(size_t size) : totalSize(size){
+    memoryPool = new char [size];
+    head=reinterpret_cast<Block*> (memoryPool);
+    head --> size = size -  sizeof(Block);
+    head--> free = true;
+    head --> next = nullptr;
+
+
+
+}
+
 
