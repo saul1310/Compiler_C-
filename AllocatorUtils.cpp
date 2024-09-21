e4de8f4381e5cdff1c287d5bd26e2507a17f960a
Saul Ifshin <97627272+saul1310@users.noreply.github.com>
2024-09-20 19:58:27 -0700

    added pointer alignment and namespace

--- AllocatorUtils.cpp
+++ AllocatorUtils.cpp
@@ -1,3 +1,13 @@
 #ifndef ALLOCATOR_UTILS_H
 #define ALLOCATOR_UTILS_H
 
+namespace AllocatorUtils {
+
+    // Aligns a given pointer to the nearest multiple of 'alignment'
+    inline std::uintptr_t AlignPointer(std::uintptr_t ptr, std::size_t alignment) {
+        std::size_t misalignment = ptr % alignment;
+        if (misalignment == 0) {
+            return ptr;
+        }
+        return ptr + (alignment - misalignment);
+    }
