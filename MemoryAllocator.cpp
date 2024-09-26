a536a24d58573142799e65ae3d1381c42fab1797
Saul Ifshin <97627272+saul1310@users.noreply.github.com>
2024-09-25 19:42:38 -0700

    function to align size of specified boundary given in argument

--- MemoryAllocator.cpp
+++ MemoryAllocator.cpp
@@ -3,3 +3,8 @@
 #include <cstdlib> 
 #include <new>      
 #include <iostream>
+
+
+std::size_t alignSize(std::size_t size, std::size_t alignment) {
+    return (size + alignment - 1) & ~(alignment - 1);
+}
